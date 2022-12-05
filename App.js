ad1cf7cd192655b191a3a534ea5de6d7d618cab1
lordRedsky <aminmuhammadrizki@gmail.com>
2022-12-05 16:43:31 +0800

    initial commit create screen folder create components folder install expo-linear-gradient add background image

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient'
+
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState(null)
+
+  const startGameHandler = (pickedNumber) => {
+    setUserNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onPickNumber={startGameHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/img/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        {screen}
+      </ImageBackground>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1
+  },
+
+  backgroundImage: {
+    opacity: 0.15
+  }
+})
