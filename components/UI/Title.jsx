f1b529c6521d7433fbc05ab71fe705f2e3dcbd1c
lordRedsky <aminmuhammadrizki@gmail.com>
2022-12-05 21:17:34 +0800

    -adding control button + and - -setup logic for generate random number between 1 and 99 -setup validation for next guest number higher or lower -setup validation for correct direction, and sent alert if user is lie

--- components/UI/Title.jsx
+++ components/UI/Title.jsx
@@ -1,6 +1,5 @@
 import { Text, StyleSheet } from "react-native";
 
-import Colors from "../constants/color";
 const Title = ({ children }) => {
     return (
         <Text style={styles.title} >{children}</Text>
@@ -14,10 +13,10 @@ const styles = StyleSheet.create({
         marginTop: 15,
         fontSize: 24,
         fontWeight: 'bold',
-        color: Colors.accent500,
+        color: 'white',
         textAlign: 'center',
         borderWidth: 2,
-        borderColor: Colors.accent500,
+        borderColor: 'white',
         padding: 12,
     }
 })
--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,15 +1,58 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { useState } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native'
 
-import Title from '../components/Title';
+import Title from '../components/UI/Title';
+import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/UI/PrimaryButton';
 
-const GameScreen = () => {
+const generateRandomBetween = (min, max, exclude) => {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+    if (randomNumber === exclude) {
+        return generateRandomBetween(min, max, exclude)
+    } else {
+        return randomNumber
+    }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+const GameScreen = ({ userNumber }) => {
+    const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
+    const nextGuessHandler = (direction) => { //? direction => 'lower' or 'greater'
+        if ((direction === 'lower' && currentGuess < userNumber) || (direction === 'higher' && currentGuess > userNumber)) {
+            Alert.alert("Don't lie!, 'You know that this is wrong...", [{ text: "Sorry!", style: "cancel" }])
+            return;
+        }
+
+        if (direction === 'lower') {
+            maxBoundary = currentGuess;
+        } else {
+            minBoundary = currentGuess + 1
+        }
+        console.log(minBoundary, maxBoundary, userNumber, '<<<');
+        const newRandomNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess)
+
+        setCurrentGuess(newRandomNumber)
+    }
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
-            {/* Guess */}
-            <View>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <View style={styles.bodyContainer}>
                 <Text>Higher or Lower ?</Text>
-                {/* + - */}
+                <View style={styles.buttonsContainer} >
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                        -
+                    </PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'higher')}>
+                        +
+                    </PrimaryButton>
+                </View>
             </View>
 
             {/* <View>LOG ROUNDS</View> */}
@@ -24,5 +67,14 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
     },
+
+    bodyContainer: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row'
+    }
 
 })
